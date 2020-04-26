8aa758b3c645773b2552367e5d2bae415564dd04
VictorYugo <vhs220796@gmail.com>
2020-04-26 18:15:23 -0500

    creacion de los componentes de cantidad y compra de productos

---
+++ src/components/CantidadProducto.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class CantidadProducto extends Component{
+    render(){
+        return (
+            <React.Fragment>
+                Unidades: 15
+            </React.Fragment>
+        )
+    }
+}
+
+export default CantidadProducto
